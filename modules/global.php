174b521741c7bdb97c1dc8af7e8945e72d284f2b
Art <121850372+arterxsk@users.noreply.github.com>
2023-06-02 02:24:29 +0800

    update modules/global.php

--- modules/global.php
+++ modules/global.php
@@ -1,6 +1,5 @@
 <?php
-$botToken = "https://api.telegram.org/bot6253592246:AAGo97eYSpSJLou40Lbd_lv-rsW0MhLsBHY"; //
-Enter ur bot token
+$botToken = "https://api.telegram.org/bot6253592246:AAGo97eYSpSJLou40Lbd_lv-rsW0MhLsBHY"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');

